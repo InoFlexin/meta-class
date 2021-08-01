9bae9aa593303c7b88212fd2d7a3ad9cfa7447e9
DaeYoung Nam <wsnam0507@naver.com>
2021-08-02 05:04:43 +0900

    test: 테스트 코드 수정

--- src/test/java/com/metaclass/test/UserTest.java
+++ src/test/java/com/metaclass/test/UserTest.java
@@ -2,7 +2,6 @@ package com.metaclass.test;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.metaclass.member.model.MemberModel;
 import com.metaclass.member.repository.MemberRepository;
 import com.metaclass.member.service.MemberService;
 import org.junit.jupiter.api.Test;
