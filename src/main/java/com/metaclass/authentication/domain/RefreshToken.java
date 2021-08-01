33633ffd140f22308b07205ce9b211e4149541fc
DaeYoung Nam <wsnam0507@naver.com>
2021-08-02 03:08:40 +0900

    feat: Refresh 토큰을 RDB에 저장하기 위한 도메인 구현

--- src/main/java/com/metaclass/authentication/domain/RefreshToken.java
+++ src/main/java/com/metaclass/authentication/domain/RefreshToken.java
@@ -1,2 +1,33 @@
-package com.metaclass.authentication.domain;public class RefreshToken {
+package com.metaclass.authentication.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "refresh_token")
+@Entity
+public class RefreshToken {
+
+    @Id
+    @Column(length = 150)
+    private String tableKey;
+
+    @Column
+    private String value;
+
+    public RefreshToken updateValue(String token) {
+        this.value = token;
+        return this;
+    }
+
+    @Builder
+    public RefreshToken(String key, String value) {
+        this.tableKey = key;
+        this.value = value;
+    }
+
 }
