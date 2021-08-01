460449718b14f751b59296c23d86db6ab99730b1
SangHoon Lee <realdevelope@naver.com>
2021-08-01 23:20:37 +0900

    test axios

--- react/meta-class-client/src/index.js
+++ react/meta-class-client/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import { Provider } from 'react-redux';
+
+App.get("api/hello", (req, res) => {
+  res.send("안녕하세요");
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +20,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
