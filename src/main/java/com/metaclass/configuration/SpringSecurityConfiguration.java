273d1440d6437735d0d3d56e6d68c5b3d380eaaa
DaeYoung Nam <wsnam0507@naver.com>
2021-08-02 03:07:22 +0900

    feat: security 설정 변경

--- src/main/java/com/metaclass/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/metaclass/configuration/SpringSecurityConfiguration.java
@@ -1,18 +1,58 @@
 package com.metaclass.configuration;
 
+import com.metaclass.authentication.JwtAccessDeniedHandler;
+import com.metaclass.authentication.JwtAuthenticationEntryPoint;
+import com.metaclass.authentication.JwtAuthenticationFilter;
+import com.metaclass.authentication.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private JwtTokenProvider tokenProvider = new JwtTokenProvider();
+    private JwtAccessDeniedHandler accessDeniedHandler;
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.httpBasic().disable()
+                .csrf().disable() // csrf 보안 토큰 disable
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler)
+                .and()
+                // jwt 토큰 기반이므로 session 사용해제
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests() //모든 요청에 대해 권한 체크
+                .antMatchers("/authentication/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
     }
 }
