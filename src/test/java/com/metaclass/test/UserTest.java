8bcba0612e29f04ce6db68b8d89db223d0200594
KCH6937 <48710060+KCH6937@users.noreply.github.com>
2021-08-01 20:59:58 +0900

    테스트코드 기능별로 분류

--- src/test/java/com/metaclass/test/UserTest.java
+++ src/test/java/com/metaclass/test/UserTest.java
@@ -18,22 +18,7 @@ public class UserTest {
 
     @Test
     public void userDomainCreateTest() {
-        MemberModel model = new MemberModel(1, "adasddasd12@naver.com", "KCH", "1234", 1);
+        MemberModel model = new MemberModel("adasddasd12@naver.com", "KCH", "1234", 1);
         memberService.memberSave(model);
-
-        Member searchMember = memberRepository.findByEmail("adasddasd12@naver.com");
-        System.out.println("search member is " + searchMember);
-//        String username = "skaeodud0507";
-//        String password = "1234124";
-//
-//        Member userInfo = new Member(username, password);
-//        memberRepository.save(userInfo);
-//
-//        Member searchMember = memberRepository.findByUsername(username);
-//        System.out.println("search member is " + searchMember);
-//
-//        assertEquals(searchMember != null, true);
-//        assertEquals(searchMember.getUsername(), "skaeodud0507");
     }
-
 }
