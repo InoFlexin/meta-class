ce32866783ac86ec848250b068318242b5e9c9d9
DaeYoung Nam <wsnam0507@naver.com>
2021-08-02 03:09:49 +0900

    feat: RefreshToken Repository 구현

--- src/main/java/com/metaclass/authentication/repository/RefreshTokenRepository.java
+++ src/main/java/com/metaclass/authentication/repository/RefreshTokenRepository.java
@@ -1,2 +1,14 @@
-package com.metaclass.authentication.repository;public class RefreshTokenRepository {
+package com.metaclass.authentication.repository;
+
+import com.metaclass.authentication.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByTableKey(String tableKey);
+
 }
