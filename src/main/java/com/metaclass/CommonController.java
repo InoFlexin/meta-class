c38d41c6f4ca75d164bd46609f75c53704375810
DaeYoung Nam <wsnam0507@naver.com>
2021-08-02 03:57:33 +0900

    fix: index.html 반환하는 controller 이름 변경

--- src/main/java/com/metaclass/CommonController.java
+++ src/main/java/com/metaclass/CommonController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ExampleController {
+public class CommonController {
 
     @GetMapping(path = "/")
     public String index() {
-        System.out.println("run");
         return "index.html";
     }
+
 }
