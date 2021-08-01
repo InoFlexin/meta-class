21e08e46235013bb26105917516b6901077b1e4b
SangHoon Lee <realdevelope@naver.com>
2021-08-01 16:38:09 +0900

    feat: define the component

--- react/meta-class-client/src/App.js
+++ react/meta-class-client/src/App.js
@@ -1,40 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//import Auth from "./hoc/Auth";
+// pages for this product
+import LandingPage from "./components/LandingPage/LandingPage"
+import LoginPage from "./components/LoginPage/LoginPage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
 
 class App extends React.Component {
 
-    constructor() {
-      super();
-
-      this.state = {
-        message: "default message",
-        url: "https://github.com"
-      };
-    }
-
-    componentDidMount() {
-      fetch('/api/hello')
-        .then(response => response.json())
-        .then(message => {
-          this.setState( { message: message.message, url: message.url } );
-        });
-    }
-
-    render() {
-      return (
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo"/>
-              <h1 className="App-title">{ this.state.message }</h1>
-              <a href={ this.state.url }>{ this.state.url }</a>
-            </header>
-            <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-          </div>
-      )
-    }
+  componentDidMount() {
+    fetch('/api/hello')
+      .then(response => response.json())
+      .then(message => { 
+        this.setState( { message: message.message, url: message.url } );
+      });
+  }
+  
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/register" component={RegisterPage} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
