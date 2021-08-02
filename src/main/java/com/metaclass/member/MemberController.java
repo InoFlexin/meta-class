45788c638a0b6f973e2a43a56d1aa803d222099e
KCH6937 <48710060+KCH6937@users.noreply.github.com>
2021-08-02 18:44:36 +0900

    feat: 멤버 관련 Controller 구현

--- src/main/java/com/metaclass/member/MemberController.java
+++ src/main/java/com/metaclass/member/MemberController.java
@@ -1,4 +1,24 @@
 package com.metaclass.member;
 
+import com.metaclass.member.domain.Member;
+import com.metaclass.member.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class MemberController {
+
+    @Autowired
+    MemberService memberService;
+
+    @GetMapping(path = "/member/{email}")   // email로 사용자 정보 찾아오기
+    public ResponseEntity<?> findByEmail(@PathVariable String email) {
+        return new ResponseEntity<>(memberService.getUser(email), HttpStatus.OK);
+    }
+
 }
