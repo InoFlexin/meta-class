2fee4df887fd7401e18868781a20fe991ada08e2
SangHoon Lee <realdevelope@naver.com>
2021-08-02 21:10:15 +0900

    feat: axios로 서버에 로그인 요청코드 작성

--- react/meta-class-client/src/components/LoginPage/LoginPage.js
+++ react/meta-class-client/src/components/LoginPage/LoginPage.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
 import { Button, Form, FormGroup, Input } from 'reactstrap';
 //import { GoogleLoginButton } from 'react-social-login-buttons';
 
 function LoginPage() {
+
+    const [id,setId] = useState('');
+    const [pw,setPw] = useState('');
+  
+  
+    useEffect(()=>{
+      console.log(localStorage.getItem("token"));
+    })
+  
+    const onChange  = (e) => {
+      const {name,value} = e.target;
+      switch(name){ 
+        case "ID":
+          return(
+            setId(value)
+          )  
+        case "PW":
+          return(
+            setPw(value)
+          )      
+        default:
+          throw new Error("It works on my computerㅋㅋ");   
+      }
+    }
+  
+    function Login(){
+      axios({
+        method: 'POST',
+        url: 'http://localhost:8080/authentication/login',
+        data: {
+          "email": id,
+          "password": pw
+        }
+      }).then((res)=>{
+        console.log(res);
+        console.log(`token : ${res.data.accessToken}`);
+        localStorage.setItem("token",res.data.accessToken);
+  
+        console.log(localStorage.getItem("token"));
+      }).catch((res)=>{
+        console.log(res);
+      })
+      
+    }
 
     return (
         <div style={{
@@ -18,18 +63,18 @@ function LoginPage() {
                     <span className="font-weight-bold">로그인</span>
                 </h1>
             <FormGroup>
-                <Input type="email" placeholder="아이디 또는 이메일"></Input>
+                <Input type="email" placeholder="아이디 또는 이메일" name="ID" value={id} onChange={onChange}></Input>
             </FormGroup>
             <FormGroup>
-                <Input type="password" placeholder="비밀번호"></Input>
+                <Input type="password" placeholder="비밀번호" name="PW" value={pw} onchange={onChange}></Input>
             </FormGroup>
             <div className="d-grid gap-2">
-                <Button type="submit" variant="secondary" size="lg" >
+                <Button type="submit" variant="secondary" size="lg" block onClick={Login} >
                     Log in
                 </Button>
-                <div className="text-center pt-3">
+                {/* <div className="text-center pt-3">
                     Or continue with your social account
-                </div>
+                </div> */}
                 {/*<GoogleLoginButton className="mt-3 mb-3"></GoogleLoginButton>*/}
                 <div className="text-center">
                     <a href="/register">Sign up</a>
