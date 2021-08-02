4d4d68269028eb5f9cf552006702fca2f2e7581a
KCH6937 <48710060+KCH6937@users.noreply.github.com>
2021-08-02 18:41:35 +0900

    fix: MemberService 수정

--- src/main/java/com/metaclass/member/service/MemberService.java
+++ src/main/java/com/metaclass/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.metaclass.member.domain.Member;
 import com.metaclass.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MemberService {
@@ -11,9 +12,16 @@ public class MemberService {
     @Autowired
     private MemberRepository memberRepository;
 
-//    public void memberSave(MemberModel model) {
-//        Member member = new Member(model.getEmail(), model.getName(), model.getPassword());
-//        memberRepository.save(member);
-//    }
+    @Transactional  // 서비스 함수가 종료될 떄 commit or rollback
+    public Member getUser(String email) {
+        return memberRepository.findByEmail(email)
+                .orElseThrow(()->new IllegalArgumentException("email을 확인해주세요"));
+    }
+
+    @Transactional
+    public String userDel(long id) {    // id 값으로 삭제
+        memberRepository.deleteById(id);
+        return "Delete Success";
+    }
 
 }
