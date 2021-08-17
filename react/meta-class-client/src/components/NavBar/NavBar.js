e050285d93fc1036e27f483ae1177606a834ea2a
SangHoon Lee <realdevelope@naver.com>
2021-08-18 00:38:16 +0900

    feat: 리다이렉트 추가

--- react/meta-class-client/src/components/NavBar/NavBar.js
+++ react/meta-class-client/src/components/NavBar/NavBar.js
@@ -1,29 +1,35 @@
-import React from 'react'
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-rouer-dom';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
 
 function NavBar() {
-    return (
-
-      <Navbar bg="white" expand="lg" fixed="top">
-        <Container>
-          <Navbar.Brand href="#/">
-            <a href="/" className="logo">
-              <span><img src="./images/logo.svg" alt="" /></span>
-              <span className="header-title" >M E T A  C L A S S</span>
-            </a>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Link to="/">Home</Link>
-              <Link to="/login">Login</Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    )
+  return (
+    <Navbar bg="white" expand="lg" fixed="top">
+      <Container>
+        <Navbar.Brand href="#/">
+          <a href="/" className="logo">
+            <span>
+              <img src="./images/logo.svg" alt="" />
+            </span>
+            <span className="header-title">M E T A C L A S S</span>
+          </a>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link to="/" className="header-menu">
+              <span>Home</span>
+            </Link>
+            <Link to="/login" className="header-menu">
+              {localStorage.getItem("X_AUTH_TOKEN") ? (<span onClick={() => {
+                    localStorage.removeItem("X_AUTH_TOKEN");}}>Log out</span>) : (<span>Login</span>)}
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default NavBar
+export default NavBar;
