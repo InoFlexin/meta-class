5190ccfc603bcc0767d963e5b49ad9b15d24ee0c
KCH6937 <adasddasd12@naver.com>
2021-08-28 04:04:00 +0900

    fileName 필드 추가, 메서드 코드 추가

--- src/main/java/com/metaclass/lesson/model/LessonRequestModel.java
+++ src/main/java/com/metaclass/lesson/model/LessonRequestModel.java
@@ -1,6 +1,6 @@
 package com.metaclass.lesson.model;
-
 import com.metaclass.lesson.domain.Lesson;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,21 +13,24 @@ public class LessonRequestModel {
     private String className;
     private String teacher;
     private String lessonName;
+    private String fileName;
 
     public Lesson toLesson() {
         return Lesson.builder()
                 .className(className)
                 .teacher(teacher)
                 .lessonName(lessonName)
+                .fileName(fileName)
                 .build();
     }
 
     @Override
     public String toString() {
-        return "LessonModel{" +
+        return "LessonRequestModel{" +
                 "className='" + className + '\'' +
                 ", teacher='" + teacher + '\'' +
                 ", lessonName='" + lessonName + '\'' +
+                ", fileName='" + fileName + '\'' +
                 '}';
     }
 }
