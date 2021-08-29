ce4aa4207f9a98aee76f3ee7b2e9a5d9df16eefd
devdynam0507 <wsnam0507@naver.com>
2021-08-29 23:46:40 +0900

    feat: error controller 추가

--- src/main/java/com/metaclass/CustomErrorController.java
+++ src/main/java/com/metaclass/CustomErrorController.java
@@ -1,2 +1,19 @@
-package com.metaclass;public class CustomErrorController {
+package com.metaclass;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "/index.html";
+    }
+
+    public String getErrorPath() {
+        return "/error";
+    }
+
 }
