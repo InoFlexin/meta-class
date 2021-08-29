4e86a7d092af5efffa9a2388f6dc6d711e410bab
devdynam0507 <wsnam0507@naver.com>
2021-08-29 23:46:58 +0900

    feat: log 추가

--- src/main/java/com/metaclass/authentication/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/metaclass/authentication/jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.metaclass.authentication.jwt;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -21,7 +23,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = tokenProvider.resolveToken((HttpServletRequest) request); // 헤더에서 토큰정보를 가져옵니다.
-
+        log.info("{}", token);
         // 토큰 유효성 검사
         if(token != null && tokenProvider.validationToken(token)) {
             // 토큰에서 유저 정보를 가져옵니다.
