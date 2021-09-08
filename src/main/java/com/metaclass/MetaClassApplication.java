9826c72a803e6327d3f057d1e6445d8db1b410f7
devdynam0507 <wsnam0507@naver.com>
2021-09-08 14:54:52 +0900

    feat: AppProperties 등록

--- src/main/java/com/metaclass/MetaClassApplication.java
+++ src/main/java/com/metaclass/MetaClassApplication.java
@@ -1,8 +1,11 @@
 package com.metaclass;
 
+import com.metaclass.configuration.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(AppProperties.class)
 @SpringBootApplication
 public class MetaClassApplication {
 
